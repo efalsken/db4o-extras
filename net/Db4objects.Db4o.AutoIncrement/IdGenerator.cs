1a1e36e85a4f7389c5992133443649121af982c6
efalsken <efalsken@158170fe-e6f2-8ae7-1763-651154b6e62f>
2010-10-12 17:26:24 +0000

    AutoIncrement now compiles.

--- net/Db4objects.Db4o.AutoIncrement/IdGenerator.cs
+++ net/Db4objects.Db4o.AutoIncrement/IdGenerator.cs
@@ -4,52 +4,42 @@ using System.Linq;
 using Db4objects.Db4o;
 using Db4objects.Db4o.Linq;
 
-namespace Gamlor.ICOODB.Db4oUtils
-{
-    internal class IdGenerator
-    {
-        private PersistedAutoIncrements state = null;
+namespace Db4objects.Db4o.AutoIncrement {
+	internal class IdGenerator {
+		private PersistedAutoIncrements state = null;
 
-        public int NextId(Type type, IObjectContainer container)
-        {
-            var incrementState = EnsureLoadedIncrements(container);
-            return incrementState.NextNumber(type);
-        }
+		public int NextId(Type type, IObjectContainer container) {
+			var incrementState = EnsureLoadedIncrements(container);
+			return incrementState.NextNumber(type);
+		}
 
-        public void StoreState(IObjectContainer container)
-        {
-            if (null != state)
-            {
-                container.Store(state);
-            }
-        }
+		public void StoreState(IObjectContainer container) {
+			if (null != state) {
+				container.Store(state);
+			}
+		}
 
-        private PersistedAutoIncrements EnsureLoadedIncrements(IObjectContainer container)
-        {
-            return state ?? (state = loadOrCreateState(container));
-        }
+		private PersistedAutoIncrements EnsureLoadedIncrements(IObjectContainer container) {
+			return state ?? (state = loadOrCreateState(container));
+		}
 
-        private static PersistedAutoIncrements loadOrCreateState(IObjectContainer container)
-        {
-            var existingState = container.Cast<PersistedAutoIncrements>().SingleOrDefault();
-            return existingState ?? new PersistedAutoIncrements();
-        }
+		private static PersistedAutoIncrements loadOrCreateState(IObjectContainer container) {
+			var existingState = container.Query<PersistedAutoIncrements>().SingleOrDefault();
+			return existingState ?? new PersistedAutoIncrements();
+		}
 
-        private class PersistedAutoIncrements
-        {
-            private readonly IDictionary<Type, int> currentHighestIds = new Dictionary<Type, int>();
+		private class PersistedAutoIncrements {
+			private readonly IDictionary<Type, int> currentHighestIds = new Dictionary<Type, int>();
 
-            public int NextNumber(Type type)
-            {
-                var number = 0;
-                if (!currentHighestIds.TryGetValue(type, out number))
-                {
-                    number = 0;
-                }
-                number += 1;
-                currentHighestIds[type] = number;
-                return number;
-            }
-        }
-    }
+			public int NextNumber(Type type) {
+				var number = 0;
+				if (!currentHighestIds.TryGetValue(type, out number)) {
+					number = 0;
+				}
+				number += 1;
+				currentHighestIds[type] = number;
+				return number;
+			}
+		}
+	}
 }
