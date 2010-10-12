1629cff0603f45f6048c5e9075b616cc0498c103
efalsken <efalsken@158170fe-e6f2-8ae7-1763-651154b6e62f>
2010-10-12 17:53:40 +0000

    Added example model class

---
+++ net/Db4objects.Db4o.AutoIncrement/ExampleClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Db4objects.Db4o.AutoIncrement;
+
+namespace Db4objects.Db4o.AutoIncrement {
+	public class ExampleClass {
+
+		[AutoIncrement]
+		public int IdAutoProperty { get; set; }
+
+		[AutoIncrement]
+		public int IdAutoProperty2 { get; private set; }
+
+		[AutoIncrement]
+		protected int _idField;
+		public int IdFieldAccessor {
+			get { return _idField; }
+			set { _idField = value; }
+		}
+	}
+}
