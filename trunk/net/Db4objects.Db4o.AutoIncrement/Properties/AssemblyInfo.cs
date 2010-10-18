cf69f4975ac55fd312b454f1a24473e452759989
gamlerhart@gmail.com <gamlerhart@gmail.com@158170fe-e6f2-8ae7-1763-651154b6e62f>
2010-10-18 09:46:09 +0000

    rs: Fixed a nasty bug where updated ids where lost

--- trunk/net/Db4objects.Db4o.AutoIncrement/Properties/AssemblyInfo.cs
+++ trunk/net/Db4objects.Db4o.AutoIncrement/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// for testing purpuses
+[assembly: InternalsVisibleTo("Db4objects.Db4o.AutoIncrement.Test")]
